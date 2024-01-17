f89167b6aeb82666cc1ced66571c8fc097b92d54
gabrielebottari <gabriele.bottari1@gmail.com>
2024-01-17 14:02:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+
+</script>
+
+<template>
+    <div>
+        <AppTitle />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+         }
+    },
+    template: `<div>count is {{ count }}</div>`,
+};
+</script>
+
+<template>
+    
+    <div>
+        <header>
+            <h1 class="main-title">
+                {{ title }}
+            </h1>
+        </header>
+    </div>
+
+</template>
+
+<style scoped>
+
+header {
+    padding: 20px;
+    background-color: aquamarine;
+}
+.main-title {
+    color: green;
+    text-align: center;   
+}
+
+</style>
